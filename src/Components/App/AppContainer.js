337438edbb754a8a3188c865e99c94e28a46901d
Lee Jeong Min <danmin20@khu.ac.kr>
2020-03-14 22:12:07 +0900

    render notifications

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -6,20 +6,24 @@ class AppContainer extends Component {
   //functions which we want to include in store(provider) should be in the constructor
   constructor(props) {
     super(props);
-    this._changeMessage = () => {
-      if (this.state.message === "hello") {
-        this.setState({
-          message: "bye"
-        });
-      } else {
-        this.setState({
-          message: "hello"
-        });
-      }
-    };
     this.state = {
-      message: "hello",
-      changeMessage: this._changeMessage
+      notifications: {
+        "1": {
+          id: 1,
+          text: "one",
+          seen: false
+        },
+        "2": {
+          id: 2,
+          text: "two",
+          seen: false
+        },
+        "3": {
+          id: 3,
+          text: "three",
+          seen: false
+        }
+      }
     };
   }
   render() {
